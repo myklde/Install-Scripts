0d87505db847b6e44dbf617eac4c30788d303871
Mika <200695570+myklde@users.noreply.github.com>
2026-03-05 20:37:00 +0100

    Update Docker installation script for Debian 13

--- Docker-install-script-Debian-13.sh
+++ Docker-install-script-Debian-13.sh
@@ -4,12 +4,12 @@ apt update && apt upgrade -y
 
 apt install -y sudo curl ca-certificates
 
-# Dockers offiziellen GPG-Schlüssel hinzufügen
+# Add Docker's official GPG key
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
-# Docker Repository hinzufügen
+# Add Docker repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
@@ -17,17 +17,17 @@ echo \
 
 apt update
 
-# Docker installieren
+# Install Docker
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Docker starten
+# Start Docker
 systemctl start docker 2>/dev/null || service docker start 2>/dev/null || dockerd > /dev/null 2>&1 &
 
-# Testcontainer
+# Test container
 docker run hello-world
 
-# User zur Docker-Gruppe hinzufügen
+# Add user to the Docker group
 usermod -aG docker $USER
 
-# Gruppe neu laden
+# Reload group
 newgrp docker
