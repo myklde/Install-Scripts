7dc9aeba0f4bdeefd471a233b6a45818ed024aa5
Mika <200695570+myklde@users.noreply.github.com>
2026-03-06 18:45:02 +0100

    Update OpenCode-install-script-Debian-13.sh

--- OpenCode-install-script-Debian-13.sh
+++ OpenCode-install-script-Debian-13.sh
@@ -140,7 +140,7 @@ verify_installation() {
     fi
     
     echo -e "\n${GREEN}=== Installation completed! ===${NC}\n"
-    echo " > REBOOT and Start OpenCode with: opencode < "
+    echo "     REBOOT one time and Start OpenCode in the Terminal with:   opencode    "
 }
 
 main() {
