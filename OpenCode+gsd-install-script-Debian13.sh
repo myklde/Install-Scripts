6b7b64cc3197d300027f14d73d8ff5e23772ac7f
Mika <200695570+myklde@users.noreply.github.com>
2026-09-10 21:31:54 +0200

    Update OpenCode+gsd-install-script-Debian13.sh

--- OpenCode+gsd-install-script-Debian13.sh
+++ OpenCode+gsd-install-script-Debian13.sh
@@ -5,8 +5,10 @@
 # Debian 13
 #
 # Erstinstallation:
-#   apt update && apt install -y curl && curl -fsSL https://raw.githubusercontent.com/myklde/Install-Scripts/main/OpenCode-install-script-Debian-13.sh | bash
+
+#  -------->                apt update && apt install -y curl && curl -fsSL https://raw.githubusercontent.com/myklde/Install-Scripts/main/OpenCode-install-script-Debian-13.sh | bash
 #
+
 # Update:
 #   ./install.sh update
 #
