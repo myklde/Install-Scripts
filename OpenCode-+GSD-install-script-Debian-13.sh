a8188f46391f5b48693f4b0d86645a82a212d4ea
Mika <200695570+myklde@users.noreply.github.com>
2026-09-10 22:37:44 +0200

    Update and rename OpenCode-(+GSD)-install-script-Debian-13.sh to OpenCode-+GSD-install-script-Debian-13.sh

--- OpenCode-+GSD-install-script-Debian-13.sh
+++ OpenCode-+GSD-install-script-Debian-13.sh
@@ -6,7 +6,7 @@
 
 
 # INSTALL THE SCRIPT WITH THIS COMMAND: 
-# ->     apt update && apt install curl -y && curl -sSL -o install.sh https://raw.githubusercontent.com/myklde/Install-Scripts/main/OpenCode-(+GSD)-install-script-Debian-13.sh && chmod +x install.sh && ./install.sh
+# ->     apt update && apt install curl -y && curl -sSL -o install.sh https://raw.githubusercontent.com/myklde/Install-Scripts/main/OpenCode+GSD-install-script-Debian-13.sh && chmod +x install.sh && ./install.sh
 
 
 
